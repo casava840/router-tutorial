ba40083ef38302c62c23361168df98dcbd51b0ae
casava840 <hen3403@gmail.com>
2024-02-07 09:38:25 +0900

    add Link component to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Home from './Home';
 import logo from './logo.svg';
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import About from './About';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' Component={Home} />
-      <Route path='/about' Component={About} />
-    </Routes>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
+      <Routes>
+        <Route path='/' Component={Home} />
+        <Route path='/about' Component={About} />
+      </Routes>
+    </div>
   );
 }
 
